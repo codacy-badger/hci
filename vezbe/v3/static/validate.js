0e4cb2a2dd97e076fdbf64c33ddd2c168cfc779c
Marko Medic <mrkmdc@gmail.com>
2018-10-22 02:42:43 +0200

    fixed issues

--- vezbe/v3/static/validate.js
+++ vezbe/v3/static/validate.js
@@ -1,30 +1,4 @@
-let regForm = document.getElementById('regform');
-
-regForm.addEventListener('keyup', (event) => {
-    if (event.srcElement.id === this.pass.id || event.srcElement.id === this.passrepeat.id) {
-        if (this.passrepeat.isValid())
-        this.passrepeat.setCustomValidity('');
-        this.passmessage.innerHTML = '';
-        
-        if (this.pass.value !== this.passrepeat.value) {
-            this.passrepeat.setCustomValidity('Lozinke se ne poklapaju');
-            this.passmessage.innerHTML = 'Lozinke se ne poklapaju!';
-        }
-    }
-
-    if (event.srcElement.id === this.birthdate.id) {
-        this.birthdate.setCustomValidity('');
-        let age = getAgeFromBirthDate(this.birthdate);
-        if (age < 18) {
-            this.birthdate.setCustomValidity('Morate imati vise od 18 godina da bi ste se registrovali...');
-            return;
-        }
-    }
-});
-
-regForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-});
+let regForm = document.getElementById("regform");
 
 function getAgeFromBirthDate(birthdate) {
     birthdate = new Date(birthdate.value);
@@ -32,3 +6,30 @@ function getAgeFromBirthDate(birthdate) {
 
     return Math.abs(diff.getUTCFullYear() - 1970);
 }
+
+regForm.addEventListener("keyup", function(event) {
+    if (event.srcElement.id === this.pass.id || event.srcElement.id === this.passrepeat.id) {
+        if (this.passrepeat.isValid()) {
+            this.passrepeat.setCustomValidity("");
+            this.passmessage.innerHTML = "";
+        }
+        
+        if (this.pass.value !== this.passrepeat.value) {
+            this.passrepeat.setCustomValidity("Lozinke se ne poklapaju");
+            this.passmessage.innerHTML = "Lozinke se ne poklapaju!";
+        }
+    }
+
+    if (event.srcElement.id === this.birthdate.id) {
+        this.birthdate.setCustomValidity("");
+        let age = getAgeFromBirthDate(this.birthdate);
+        if (age < 18) {
+            this.birthdate.setCustomValidity("Morate imati vise od 18 godina da bi ste se registrovali...");
+            return;
+        }
+    }
+});
+
+regForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
